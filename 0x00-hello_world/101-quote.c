ac3b958646265d4d40d6b618cd1de71c2a866a6a
jephthah40 <edemjephthah1@gmail.com>
2023-03-16 09:16:37 -0700

    My commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #define STDOUT_FILENO 1
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
@@ -8,7 +9,7 @@
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-	return (1);
+        write(STDOUT_FILENO,
+        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+        return (1);
 }
