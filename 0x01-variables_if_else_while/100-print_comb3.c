f3d7489799ceffceed4d796aaed7ead5e0ae48c1
jephthah40 <edemjephthah1@gmail.com>
2023-03-17 06:10:48 -0700

    My commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,12 +10,12 @@ int main(void)
 {
 	int d;
 
-	for (d = 0; d < 90; d++)
-	{
+   for (d = 0; d < 90; d++)
+   {
 	
 		
-			putchar((d / 10) + '0');
-			putchar((d % 10) + '0');
+      putchar((d / 10) + '0');
+      putchar((d % 10) + '0');
 		
 		if (d != 89)
 		{
